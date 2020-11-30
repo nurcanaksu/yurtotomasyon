b6940ffca4569e4c6f7faa41c331097dda9acebe
nurcanaksu <63795619+nurcanaksu@users.noreply.github.com>
2020-11-30 17:00:49 +0300

    Add files via upload

---
+++ dilekistekclass.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nurcan
+ */
+public class dilekistekclass
+{
+   private int id;
+   private String dilek;
+
+
+  public dilekistekclass(int id, String dilek)
+  {
+        this.id = id;
+        this.dilek= dilek;
+  }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDilek() {
+        return dilek;
+    }
+
+    public void setDilek(String dilek) {
+        this.dilek = dilek;
+    }
+
+  
+  
+  
+  
+}
---
+++ duyuruclass.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nurcan
+ */
+public class duyuruclass {
+    private int id;
+    private String duyuru;
+
+    public duyuruclass(int id, String duyuru) {
+        this.id = id;
+        this.duyuru = duyuru;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+   
+
+    public String getDuyuru() {
+        return duyuru;
+    }
+
+    public void setDuyuru(String duyuru) {
+        this.duyuru = duyuru;
+    }
+
+  
+    
+}
